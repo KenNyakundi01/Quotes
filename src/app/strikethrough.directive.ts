6a44bfcde16532981a81191bd9df01c8e80a963c
Kenn <sabatojnr01@gmail.com>
2020-06-15 11:33:58 +0300

    modification

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -4,8 +4,7 @@ import { Directive,ElementRef,HostListener} from '@angular/core';
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
-
-  constructor(private elem:ElementRef){}
+ constructor(private elem:ElementRef){}
 
   @HostListener("click") onClicks(){
     this.textDeco("line-through")
